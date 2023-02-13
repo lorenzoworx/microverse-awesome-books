949d4ef654cfe213244437cedc19f64bbec1803f
Precious Betine <preciousbetine@gmail.com>
2023-02-13 14:13:03 +0100

    Add styling

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-const books = [];
+const books = [
+  {
+    title: 'Hello',
+    author: 'Precious'
+  },
+  {
+    title: 'Hello',
+    author: 'Precious'
+  },
+  {
+    title: 'Hello',
+    author: 'Precious'
+  },
+];
 
 function addBook(title, author) {
   books.push({ id: books.length, title, author });
@@ -8,3 +21,20 @@ function removeBook(bookId) {
   const bookIndex = books.findIndex((book) => book && book.id == bookId);
   delete books[bookIndex];
 }
+
+function showBooks() {
+  let booksHTML = '';
+  books.forEach((book) => {
+    booksHTML += `
+      <div class="book">
+        <h3>${book.title}</h3>
+        <p>${book.author}</p>
+        <button>Remove</button>
+      </div>
+    `;
+  });
+  const booksContainer = document.querySelector('#books_container');
+  booksContainer.innerHTML = booksHTML;
+}
+
+showBooks();
